814baf3bf91435ad73c8dff73ed616f908ec0d3e
erfan gharib <erfangharib5@gmail.com>
2022-02-24 10:39:24 +0330

    update: styles - package.json - tailwind config - add font file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,23 @@
 module.exports = {
   purge: [
-    './pages/**/*.html',
-    './pages/**/**/*.html'
+    './pages/**/*.{html,js}',
+    './components/**/*.{html,js}',
   ],
   content: [],
   theme: {
     extend: {
       width: {
         '12/13':'95%'
+      },
+      minHeight: {
+        64:'256px'
+      },
+      colors: {
+        headLine:'#222',
+        secondary: '#b1b1b1',
+        bg: '#f7f7f7'
       }
+
     },
   },
   plugins: [],
